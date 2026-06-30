a8d42d531736d3eef372454b5c42a64e421e5798
Syed Shariful Alam Opu <shorif245@gmail.com>
2026-06-30 14:21:41 +0600

    Fix CI: Make Celery broker/backend hosts configurable

--- celery_config.py
+++ celery_config.py
@@ -1,10 +1,15 @@
+import os
 from celery import Celery
 
 # Create Celery app instance
 app = Celery('taskflow')
 
+# Allow override via env vars (for CI), default to Docker service names
+rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')
+redis_host = os.getenv('REDIS_HOST', 'redis')
+
 # Connect to RabbitMQ (message broker)
-app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672//'
+app.conf.broker_url = f'amqp://guest:guest@{rabbitmq_host}:5672//'
 
 # Store results in Redis (result backend)
-app.conf.result_backend = 'redis://redis:6379/0'
+app.conf.result_backend = f'redis://{redis_host}:6379/0'
